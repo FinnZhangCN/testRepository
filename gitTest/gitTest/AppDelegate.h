9d69f6bfc017fb9679a4111b413f4ddf71cbc975
Finn Zhang <FinnZhang@FinndeMacBook-Pro.local>
2016-12-01 09:39:03 +0800

    初始化项目

---
+++ gitTest/gitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by Finn Zhang on 2016/12/1.
+//  Copyright © 2016年 Finn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitTest/gitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitTest
+//
+//  Created by Finn Zhang on 2016/12/1.
+//  Copyright © 2016年 Finn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
