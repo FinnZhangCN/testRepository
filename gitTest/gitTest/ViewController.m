fa4063fd877297d8c010dcf732df02caaf850c4c
Finn Zhang <FinnZhang@FinndeMacBook-Pro.local>
2016-12-01 15:43:22 +0800

    test  AFN 下载功能 基本使用

--- gitTest/gitTest/ViewController.m
+++ gitTest/gitTest/ViewController.m
@@ -24,8 +24,8 @@
     [self.view addSubview:imageView];
     
     // test cocoapods AFN测试
-    // 上传操作
-    [self upload];
+    // 上传的基本使用
+    // 下载的基本使用
 }
 
 - (void)upload {
@@ -48,6 +48,35 @@
         NSLog(@"error ---%@",error);
     }];
 }
+
+- (void)download {
+    // 创建会话管理
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    // 创建请求
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://4493bz.1985t.com/uploads/allimg/150127/4-15012G52133.jpg"]];
+    // 发送请求   注意! 该方法会返回一个NSURLSessionDownloadTask对象
+    NSURLSessionDownloadTask *task = [manager downloadTaskWithRequest:request progress:^(NSProgress * _Nonnull downloadProgress) {
+        //downloadProgress内部的属性中有已下载内容,以及目标文件内容,故很方便的监听到下载进度
+        NSLog(@"%f",1.0 * downloadProgress.completedUnitCount / downloadProgress.totalUnitCount);
+    } destination:^NSURL * _Nonnull(NSURL * _Nonnull targetPath, NSURLResponse * _Nonnull response) {
+        // 此block块需要返回文件存放的目标路径,内部会自动实现剪切操作
+        // 文件名
+        NSString *fileName = [response suggestedFilename];
+        // cache路径
+        NSString *cache = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES).lastObject;
+        // 全路径
+        NSString *fullPath = [cache stringByAppendingPathComponent:fileName];
+        // 返回路径
+        return [NSURL fileURLWithPath:fullPath];
+    } completionHandler:^(NSURLResponse * _Nonnull response, NSURL * _Nullable filePath, NSError * _Nullable error) {
+        // 下载完成后的回调
+        NSLog(@"path--%@",filePath);
+    }];
+    
+    // 需要手动开启!
+    [task resume];
+}
+
 
 
 @end
